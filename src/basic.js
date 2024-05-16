ffb7b47cfaed789d44ace90bb238356665a3444e
GreatHayat <greathayat@gmail.com>
2024-05-16 13:59:40 +0500

    set a prompt and created a basic chain using LCEL

--- src/basic.js
+++ src/basic.js
@@ -1,12 +1,30 @@
 import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 
+// initialize your llm model
 const llm = new ChatOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
   model: "gpt-3.5-turbo",
   temperature: 0.2,
 });
 
+// define your prompt template, you can also use fromTemplate method
+const prompt = ChatPromptTemplate.fromMessages([
+  [
+    "system",
+    "You are helpful AI assistant to write a short summary for a given topic.",
+  ],
+  ["human", "{topic}"],
+]);
+
 (async () => {
-  const response = await llm.invoke("Write 2 liner summary about NodeJS");
+  // Now we will create a basic chain, it will combine the prompt with the llm model
+  // We will use LangChain LCEL syntax to create the chain, pipe method will be used to create the chain
+  // prompt -> llm model -> response
+  const chain = prompt.pipe(llm);
+
+  // Please keep in mind that, the name of your key in the invoke method should be same
+  // as you use in the prompt within {} braces.
+  const response = await chain.invoke({ topic: "Docker" });
   console.log(response.content);
 })();
